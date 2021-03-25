f61edfb1917027a2d62960863b94a8c2fe46cfe3
Dexter <51228050+ThisIsDexter@users.noreply.github.com>
2021-03-25 10:07:54 +0530

    Update NodeMCU_HTML_Webpage.ino

--- NodeMCU_HTML_Webpage.ino
+++ NodeMCU_HTML_Webpage.ino
@@ -1,80 +1,70 @@
 /* Created by Priyam Mehta */
 
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
- 
-// Replace with your network credentials
-const char* ssid = "iot_projext";
-const char* password = "1234567890";
- 
-ESP8266WebServer server(80);   //instantiate server at port 80 (http port)
 
+WiFiClient client;
+WiFiServer server(80);
+//
+//#define led D5
+//#define led2 D6
 
+int led = 5;
+int led2 = 6;
 
-IPAddress    apIP(10, 10, 10, 1);     
-
- 
-String page = "";
-int LEDPin = 16;
-int LEDPin2 = 5;
-
-void setup(void){
-  //the HTML of the web page
-  page = "<h1>Simple NodeMCU Web Server</h1><p><a href=\"LEDOn\"><button>ON</button></a>&nbsp;<a href=\"LEDOff\"><button>OFF</button></a></p><p><a href=\"LEDOn2\"><button>Turn LED2 ON</button></a>&nbsp;<a href=\"LEDOff2\"><button>Turn LED2 OFF</button></a></p>";
-  //make the LED pin output and initially turned off
-  pinMode(LEDPin, OUTPUT);
-  pinMode(LEDPin2, OUTPUT);
-  digitalWrite(LEDPin, LOW);
-   
-  delay(1000);
-  Serial.begin(115200);
-  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));   // subnet FF FF FF 00
-  WiFi.softAP(ssid, password); //begin WiFi access point
-  Serial.println("");
- 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  WiFi.begin("iot", "project1234");
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    delay(200);
+    Serial.print("..");
   }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.softAPIP()); 
-   
-  server.on("/", [](){
-    server.send(200, "text/html", page);
-  });
-  server.on("/LEDOn", [](){
-    server.send(200, "text/html", page);
-    digitalWrite(LEDPin, HIGH);
-    Serial.println("command received");
-    delay(1000);
-  });
-    server.on("/LEDOn2", [](){
-    server.send(200, "text/html", page);
-    digitalWrite(LEDPin2, HIGH);
-    Serial.println("command received");
-    delay(1000);
-  });
-    server.on("/LEDOff2", [](){
-    server.send(200, "text/html", page);
-    digitalWrite(LEDPin2, LOW);
-    Serial.println("command received");
-    delay(1000);
-  });
-  server.on("/LEDOff", [](){
-    server.send(200, "text/html", page);
-    digitalWrite(LEDPin, LOW);
-    Serial.println("command received");
-    delay(1000); 
-  });
+  Serial.println();
+  Serial.println("NodeMCU is connected!");
+  Serial.println(WiFi.localIP());
   server.begin();
-  Serial.println("Web server started!");
+  pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
 }
- 
-void loop(void){
-  server.handleClient();
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  client = server.available();  //Gets a client that is connected to the server and has data available for reading.    
+  if (client == 1)
+  {  
+    String request =  client.readStringUntil('\n');
+    Serial.println(request);
+    request.trim();
+    if(request == "GET /led1on HTTP/1.1")
+    {
+      digitalWrite(led, HIGH);
+    }
+    if(request == "GET /led1off HTTP/1.1")
+    {
+      digitalWrite(led, LOW);
+    }
+    if(request == "GET /led2on HTTP/1.1")
+    {
+      digitalWrite(led2, HIGH);
+    }
+    if(request == "GET /led2off HTTP/1.1")
+    {
+      digitalWrite(led2, LOW);
+    }
+  }
+  client.println("Content-Type: text/html");
+  client.println("");
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+  client.println("<h1>Welcome to the Webpage!</h1>");
+  client.println("<h3>LED Controls<h3>");
+  client.println("<br>");
+  client.println("<a href=\"/led1on\"\"><button>LED 1 ON</button></a>");
+  client.println("<a href=\"/led1off\"\"><button>LED 1 OFF</button></a><br/>");
+  client.println("<a href=\"/led2on\"\"><button>LED 2 ON</button></a>");
+  client.println("<a href=\"/led2off\"\"><button>LED 2 OFF</button></a><br/>");
+  client.println("</html>");
 }
